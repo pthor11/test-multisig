a7824a66cbc757b12903db8563b2cea4da8348a8
Hamachan <phuonglm@me.com>
2020-09-15 07:24:08 +0700

    send 1-1 psbt

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { ECPair, networks, payments, Psbt } from "bitcoinjs-lib";
+import { accounts } from "./accounts";
 
 const network = networks.testnet
 
@@ -27,43 +28,40 @@ const generateAddressP2SH = (m: number, pubkeys: string[]) => {
     return address
 }
 
-// const key1 = generateAddressP2PKH()
-// const key2 = generateAddressP2PKH()
-// const key3 = generateAddressP2PKH()
+const multisigAddress = generateAddressP2SH(2, Object.keys(accounts).map(person => accounts[person].publicKey))
 
-// const keys = [key1, key2, key3]
+console.log({ multisigAddress })
 
-// const multisigAddress = generateAddressP2SH(2, keys.map(key => key.publicKey))
+const sendP2khTransactionByPsbt = async (value: number, fee: number, address: string) => {
+    const psbt = new Psbt({ network })
 
-// console.log({ keys, multisigAddress })
+    psbt.addInput({
+        hash: '18f9ac90c7a474b0d78315677065e6254b9916883e8af6dfd8d8943fa144e880',
+        index: 0,
+        nonWitnessUtxo: Buffer.from('02000000000101bb7f169155840482bfe9bc82026882dce8816fc73c8b4150c1e3190745a2878b0100000017160014aa3253ad8df3b9b6eab0923a16e3faddc03f1115feffffff02a0860100000000001976a9141c6fa80d00075917ec0cef054f29fc57fffd668e88acc5f81c000000000017a9145bf4501d292ebfdbf9d566541b74372bad8c4f3a8702473044022020972647c8e0c65574bb9d0d0b17d9350c14c9038337f17bc6addcf158c0e22402201d5f983e4e607d68a59ade6ff412c5244fea362bb3b6ff6ade9b2ef75b025f5d012103b3defd6da3bfdb59e19375db758e21cd4bdef4bfc532ccaff26c9da581ad34bc09fe1b00', 'hex')
+    })
 
+    psbt.addOutput({
+        address,
+        value
+    })
 
-const keys = [
-    {
-        address: 'mi7JyT8UAG6Ksd4LJbVuX866ssomxAZAY9',
-        privateKey: 'cVj5T5wAgKUyo367w1ezHxXrBXXnG8BL5eAbVqVceya1ikxjLDNz',
-        publicKey: '02c23eb1375eb0bf42377118c32e7a93fb497764f876e85153ca93783e07d13709'
-    },
-    {
-        address: 'mi1nxULcaCfsJJtvKEpvYfoFWcdub3UMZd',
-        privateKey: 'cRwsAPrHhntcF8bx8b3zYgES72fDZwWZ7zKjcVc6dG4i1ZmW3UxP',
-        publicKey: '02f8d3ea033f206aea7099d562f56a6427d26fe6e05e07c0b2e1a30b89d5c9034a'
-    },
-    {
-        address: 'mnw9hJ38Pq56TEZGKdGQp6wJFBQDtp4LMW',
-        privateKey: 'cQhQTcdMctHhjizWphC13gwNfjfcFGqLhhTibuEo4J2QupykUL1c',
-        publicKey: '02777447212c74fbace7453377950ea0943508239b98a1463bc462227cbda7fc5d'
-    }
-]
+    psbt.addOutput({
+        address: accounts.alice.address,
+        value: 100000 - value - fee
+    })
 
-const redeemOutput = payments.p2ms({
-    m: 2,
-    pubkeys: keys.map(key => ECPair.fromWIF(key.privateKey, network).publicKey),
-    network
-})
+    psbt.signInput(0, ECPair.fromWIF(accounts.alice.privateKey, network))
 
-console.log({ redeemOutput: redeemOutput.output?.toString('hex') });
+    psbt.validateSignaturesOfInput(0)
 
-const multisigAddress = '2Mwnxqt1ryXZ1iBHE1dgc1TseQE2bR4kWFP'
+    psbt.finalizeAllInputs()
 
-const psbt = new Psbt({ network })
+    const raw_tx = psbt.extractTransaction().toHex()
+
+    console.log({ raw_tx });
+
+}
+
+// sendP2khTransactionByPsbt(10000, 223, multisigAddress as string)
+
