90d7aa49364d68f8889d217793cfc1b458ce977b
phuonglm <phuonglm@me.com>
2020-09-22 22:50:27 +0700

    connect mongo

--- src/index.btc.ts
+++ src/index.btc.ts
@@ -1,1 +1,19 @@
-console.log(`btc up`)
+import { connectDb } from "./mongo"
+
+const start = async() => {
+    try {
+
+        await connectDb()
+        
+        const hello = () => {
+            console.log('btc')
+            setTimeout(hello, 1000)
+        }
+
+        hello()
+    } catch (e) {
+        throw e
+    }
+}
+
+start()
